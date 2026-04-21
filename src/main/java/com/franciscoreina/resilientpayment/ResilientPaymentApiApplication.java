4d77014a74ebe18bbf800d8528718c7f5ba7132f
Francisco Reina <franciscoreinaponce@gmail.com>
2026-04-21 18:31:20 +0100

    Initial commit

---
+++ src/main/java/com/franciscoreina/resilientpayment/ResilientPaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.franciscoreina.resilientpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResilientPaymentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResilientPaymentApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/franciscoreina/resilientpayment/ResilientPaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franciscoreina.resilientpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResilientPaymentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
