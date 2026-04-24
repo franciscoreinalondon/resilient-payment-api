b0ff62ac62f9a4240703950c2332e9cb0cf9af7f
Francisco Reina <franciscoreinaponce@gmail.com>
2026-04-24 12:57:32 +0100

    Add Timeout

--- src/main/java/com/franciscoreina/resilientpayment/config/RestClientConfig.java
+++ src/main/java/com/franciscoreina/resilientpayment/config/RestClientConfig.java
@@ -1,17 +1,29 @@
 package com.franciscoreina.resilientpayment.config;
 
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
+import org.springframework.boot.http.client.HttpClientSettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
     public RestClient restClient() {
+
+        var settings = HttpClientSettings.defaults()
+                .withConnectTimeout(Duration.ofSeconds(1))
+                .withReadTimeout(Duration.ofSeconds(2));
+
+        var requestFactory = ClientHttpRequestFactoryBuilder.detect()
+                .build(settings);
+
         return RestClient.builder()
                 .baseUrl("http://localhost:8080")
+                .requestFactory(requestFactory)
                 .build();
     }
-
 }
